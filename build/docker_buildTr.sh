2749009119634c40dfb36d5169bc9a2f5e33c776
donggyu904 <89078731+donggyu904@users.noreply.github.com>
2024-07-09 23:47:21 +0900

    dw
    dw

--- build/docker_buildTr.sh
+++ build/docker_buildTr.sh
@@ -3,6 +3,7 @@ set -e
 set -x
 
 docker build \
+  --platform linux/arm64/v8 \
   --pull \
   --tag point2cadtr \
   -f DockerfileTr \
--- build/entrypointTr.sh
+++ build/entrypointTr.sh
@@ -3,11 +3,24 @@ if [ ! -z "${DEBUG}" ]; then
     set -x
 fi
 
-USER_ID=$(stat -c %u /work/point2cad)
-GROUP_ID=$(stat -c %g /work/point2cad)
+USER_ID=$(stat -c %u /work/point2cad) 
+GROUP_ID=$(stat -c %g /work/point2cad) 
 
-# groupadd -g $GROUP_ID usergroup
-# useradd -m -l -u $USER_ID -g usergroup user
+USER_ID=99
+GROUP_ID=99
+
+echo ${USER_ID}
+echo ${GROUP_ID}
+
+# 그룹이 이미 존재하는지 확인하고, 없다면 생성
+
+groupadd -g $GROUP_ID usergroup
+
+
+
+useradd -m -l -u $USER_ID -g $GROUP_ID user
+
+
 
 if [ ! -z "${DEBUG}" ]; then
     env
@@ -21,6 +34,5 @@ if [ ! -z "${DEBUG}" ]; then
     gosu user python -c "import torch; print(torch.cuda.is_available())"
     gosu user ls -l /dev/nvidia*
 fi
-
-# exec gosu user "$@"
-exec "$@"
+exec gosu user "$@"
+# exec "$@"
